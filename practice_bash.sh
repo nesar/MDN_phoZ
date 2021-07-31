5f063baf7a6bbccffe033cfe3527c678ac0634ba
acossairt <75052387+acossairt@users.noreply.github.com>
2021-07-30 20:02:27 -0600

    Delete practice_bash.sh

--- practice_bash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-echo "Hello World!"
-
-for this_sim in 'irac' 'des' 'irac' 'jwst' 'lsst' 'pau' 'sdss' 'wise'
-do
-    echo "$this_sim plus some more words" | tee training_plots/practice_$this_sim.txt
-done
